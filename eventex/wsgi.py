50414c3a60e7da4643e2accb911ca40dc48ad061
Bruno Dias <rj.megarj@gmail.com>
2018-06-11 00:35:52 -0300

    Fix import change CLing to Cling

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import CLing
+
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 
